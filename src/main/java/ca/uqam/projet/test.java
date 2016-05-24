87083c5899bdbc2b82c58ff8ace8a785f519569c
Philippe <philippe.deslongchamps@gmail.com>
2016-05-24 18:00:06 -0400

    rajouter premier tests bixi

--- src/main/java/ca/uqam/projet/test.java
+++ src/main/java/ca/uqam/projet/test.java
@@ -1,14 +1,7 @@
 
 package ca.uqam.projet;
 
-import ca.uqam.projet.repositories.FoodtruckRepository;
-
-import ca.uqam.projet.tasks.FetchFoodtrucksTask;
-import org.slf4j.*;
-import ca.uqam.projet.schema.CuisineDeRueSchema;
 import ca.uqam.projet.schema.StationsSchema;
-import org.apache.log4j.spi.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,8 +13,14 @@ import org.springframework.web.client.RestTemplate;
 public class test {
 
     public static void main(String args[]){
+
+        RestTemplate restTemplate=new RestTemplate();
+        StationsSchema stations=restTemplate.getForObject("https://secure.bixi.com/data/stations.json", StationsSchema.class);
+        System.out.println(stations.getStations().get(8).toString());
+
+/*
         FetchFoodtrucksTask fetch = new FetchFoodtrucksTask();
-        fetch.execute();
+        fetch.execute();*/
 
     }
 }
